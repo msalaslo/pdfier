d93f7cfeb532e764fd11d267a720e81875da015c
msalaslo <msalaslo@gmail.com>
2017-11-10 17:45:33 +0100

    megarefactoring cleaning

---
+++ pdfa-app/src/main/java/com/pdfa/msl/webaj/configuration/WebSecurityConfigurationApplication.java
@@ -0,0 +1,77 @@
+package com.pdfa.msl.webaj.configuration;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pdfa.msl.webaj.filter.CsrfHeaderFilter;
+
+@SpringBootApplication
+@RestController
+@ComponentScan ("com.pdfa.msl")
+public class WebSecurityConfigurationApplication {
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic()
+			.and().authorizeRequests().antMatchers("/", "/index", "/*").permitAll().anyRequest().authenticated()
+			.and().formLogin().loginPage("/login").permitAll()
+            .and().logout().permitAll()
+			.and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+			http.csrf().csrfTokenRepository(csrfTokenRepository())
+			;
+		}
+	}
+
+	private static CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@RequestMapping("/token")
+	@ResponseBody
+	public Map<String, String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World from app");
+		return model;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecurityConfigurationApplication.class, args);
+	}
+
+}
