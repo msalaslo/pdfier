4a668d1cdd36f9aaf7507ada068516289b81c5be
msalaslo <msalaslo@gmail.com>
2017-11-10 18:57:29 +0100

    refactoring

---
+++ pdfier-app/src/main/java/com/pdfier/security/AccessControlService.java
@@ -0,0 +1,22 @@
+package com.pdfier.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccessControlService {
+  public AccessControlService() {}
+  
+  public boolean validReferrer(String referrer) { if ((referrer != null) && ((referrer.startsWith("http://pdfier.com")) || (referrer.startsWith("http://www.pdfier.com")) || (referrer.startsWith("http://localhost/")))) {
+      return true;
+    }
+    return false;
+  }
+  
+  public boolean validLisence(String lisence)
+  {
+    if ((lisence != null) && (lisence.equals("Y2FuZGVsYWNpcmNlcm9zZW5kbw=="))) {
+      return true;
+    }
+    return false;
+  }
+}
