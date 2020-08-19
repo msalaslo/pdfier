0fb33e9f2e92782ce0ed35cfaff961394c77ca22
msalas <msalas@vectoritcgroup.com>
2020-08-19 17:51:25 +0200

    format

--- pdfier-app/src/main/java/com/pdfa/msl/webaj/filter/LisenceAccessControlFilter.java
+++ pdfier-app/src/main/java/com/pdfa/msl/webaj/filter/LisenceAccessControlFilter.java
@@ -12,27 +12,24 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.pdfier.security.AccessControlService;
 
-public class LisenceAccessControlFilter
-	extends OncePerRequestFilter
-	{
-	  @Autowired
-	  AccessControlService accessControlService;
-	  
-	  public LisenceAccessControlFilter() {}
-	  
-	  public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-	    throws IOException, ServletException
-	  {
-	    String lisence = request.getParameter("lisence");
-	    if (lisence != null) {
-	      if (accessControlService.validLisence(lisence))
-	      {
-	        chain.doFilter(request, response);
-	      } else {
-	        response.sendError(402, "No valid lisence");
-	      }
-	    } else {
-	      response.sendError(400, "lisence not found in the request");
-	    }
-	  }
+public class LisenceAccessControlFilter extends OncePerRequestFilter {
+	@Autowired
+	AccessControlService accessControlService;
+
+	public LisenceAccessControlFilter() {
 	}
+
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String lisence = request.getParameter("lisence");
+		if (lisence != null) {
+			if (accessControlService.validLisence(lisence)) {
+				chain.doFilter(request, response);
+			} else {
+				response.sendError(402, "No valid lisence");
+			}
+		} else {
+			response.sendError(400, "lisence not found in the request");
+		}
+	}
+}
